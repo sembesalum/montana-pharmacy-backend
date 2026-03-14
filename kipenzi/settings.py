f8838d359f7fabf80abfe7a43f042b840a63759f
salumsembe <salumrashidjr@gmail.com>
2026-03-14 13:40:02 +0300

    production deployment 3

--- kipenzi/settings.py
+++ kipenzi/settings.py
@@ -74,7 +74,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
-    "dev.admin-montanapharmacy.com",
     "https://dev.admin-montanapharmacy.com",
     "https://admin-montanapharmacy.com",
     "https://www.admin-montanapharmacy.com",
